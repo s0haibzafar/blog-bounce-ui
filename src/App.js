90ca74c3a728de18b50df83ed5fbb08e73e45f69
S0haibZafar <sohaibzafar71@gmail.com>
2023-12-16 21:48:22 +0500

    Use react-router-dom & create basic navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-// import './App.css';
 import Navbar from "./components/navbar/navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/footer/footer";
+import Home from "./pages/home/home";
 
 function App() {
   return (
     <>
       <div>
-        <h1>Hello World</h1>
+        <BrowserRouter>
+         <div>
+            <Navbar />
+
+            <Routes>
+              <Route
+               path="/"
+               exact
+               element={<Home />}
+              />
+
+            </Routes>
+             
+            <Footer />
+         </div>
+        </BrowserRouter>
       </div>
-      <Navbar />
     </>
   );
 }
