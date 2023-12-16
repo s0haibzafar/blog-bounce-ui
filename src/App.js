280e356f61b1efc8f94ec48d859704b3e424ff5f
S0haibZafar <sohaibzafar71@gmail.com>
2023-12-16 21:52:37 +0500

    Change home page route name

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
             <Routes>
               <Route
-               path="/"
+               path="/home"
                exact
                element={<Home />}
               />
