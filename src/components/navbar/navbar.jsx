771dc1c322c68d7199e69ed18cda010895a1ea2c
S0haibZafar <sohaibzafar71@gmail.com>
2023-12-18 22:59:34 +0500

    Set text alignment

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,19 +1,19 @@
 import { NavLink } from "react-router-dom";
 import styles from './navbar.module.css'
 
-function Navbar(){
-    return(
+function Navbar() {
+    return (
         <>
-        <nav className={styles.navbar}>
-            <NavLink to='/' className={`${styles.logo} ${styles.inActiveStyle}`} >BlogBounce</NavLink>
-            <NavLink to='/' className={({isActive})=> isActive ? styles.activeStyle : styles.inActiveStyle } >Home</NavLink>
-            <NavLink to='crypto' className={({isActive})=> isActive ? styles.activeStyle : styles.inActiveStyle } >Cryptocurrencies</NavLink>
-            <NavLink to='blogs' className={({isActive})=> isActive ? styles.activeStyle : styles.inActiveStyle }>Blogs</NavLink>
-            <NavLink to='submit' className={({isActive})=> isActive ? styles.activeStyle : styles.inActiveStyle }>Submit Blog</NavLink>
-            <NavLink to='login' className={({isActive})=> isActive ? styles.activeStyle : styles.inActiveStyle }>LogIn</NavLink>
-            <NavLink to='register' className={({isActive})=> isActive ? styles.activeStyle : styles.inActiveStyle }>Register</NavLink>
-        </nav>
-        <div></div>
+            <nav className={styles.navbar}>
+                <NavLink to='/' className={`${styles.logo} ${styles.inActiveStyle}`} >BlogBounce</NavLink>
+                <NavLink to='/' className={({ isActive }) => isActive ? styles.activeStyle : styles.inActiveStyle} >Home</NavLink>
+                <NavLink to='crypto' className={({ isActive }) => isActive ? styles.activeStyle : styles.inActiveStyle} >Cryptocurrencies</NavLink>
+                <NavLink to='blogs' className={({ isActive }) => isActive ? styles.activeStyle : styles.inActiveStyle}>Blogs</NavLink>
+                <NavLink to='submit' className={({ isActive }) => isActive ? styles.activeStyle : styles.inActiveStyle}>Submit Blog</NavLink>
+                <NavLink to='login' className={({ isActive }) => isActive ? styles.activeStyle : styles.inActiveStyle}>LogIn</NavLink>
+                <NavLink to='register' className={({ isActive }) => isActive ? styles.activeStyle : styles.inActiveStyle}>Register</NavLink>
+            </nav>
+            <div></div>
         </>
     );
 }
