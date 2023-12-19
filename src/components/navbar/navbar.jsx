e05e11e8561d2218ce868aad853fe17cfcd70879
S0haibZafar <sohaibzafar71@gmail.com>
2023-12-19 22:47:26 +0500

    Set login, register and logout btn styling

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,6 +2,7 @@ import { NavLink } from "react-router-dom";
 import styles from './navbar.module.css'
 
 function Navbar() {
+    const isAuthenticated = false;
     return (
         <>
             <nav className={styles.navbar}>
@@ -10,12 +11,18 @@ function Navbar() {
                 <NavLink to='crypto' className={({ isActive }) => isActive ? styles.activeStyle : styles.inActiveStyle} >Cryptocurrencies</NavLink>
                 <NavLink to='blogs' className={({ isActive }) => isActive ? styles.activeStyle : styles.inActiveStyle}>Blogs</NavLink>
                 <NavLink to='submit' className={({ isActive }) => isActive ? styles.activeStyle : styles.inActiveStyle}>Submit Blog</NavLink>
-                <NavLink to='login' className={({ isActive }) => isActive ? styles.activeStyle : styles.inActiveStyle}>
+                { isAuthenticated ?  
+                  <div>
+                    <NavLink to='logout'>
+                     <button className={styles.logOutBtn} >Log Out</button>
+                    </NavLink>
+                  </div>
+                : <div><NavLink to='login' className={({ isActive }) => isActive ? styles.activeStyle : styles.inActiveStyle}>
                     <button className={styles.logInBtn} >LogIn</button>
-                </NavLink>
-                <NavLink to='register' className={({ isActive }) => isActive ? styles.activeStyle : styles.inActiveStyle}>
+                    </NavLink>
+                    <NavLink to='register' className={({ isActive }) => isActive ? styles.activeStyle : styles.inActiveStyle}>
                     <button className={styles.registerBtn} >Register</button>
-                </NavLink>
+                </NavLink></div>}
             </nav>
             <div></div>
         </>
