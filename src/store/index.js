f8c5bd8d92b1fc277bd2fd2298e74e97d2eb7455
S0haibZafar <sohaibzafar71@gmail.com>
2023-12-25 18:54:05 +0500

    Configure store

--- src/store/index.js
+++ src/store/index.js
@@ -1,37 +1,8 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
+import user from './userSlice'
 
-const initilaState = {
-    _id: '',
-    email: '',
-    username: '',
-    auth: false
-}
-
-export const userSlice = createSlice({
-    name: 'user',                               // use this name when we import userSlice in other file
-    initialState: initilaState,
-    reducers: {
-        setUser: (state, action) => {
-            //when data come form api and we can get api data by action
-            const { _id, email, username, auth } = action.payload;
-
-            //now we will date state
-            state._id = _id;
-            state.email = email;
-            state.username = username;
-            state.auth = auth;
-
-        },
-        resetUser: (state, action) => {
-            //now we will reset state
-            state._id = '';
-            state.email = '';
-            state.username = '';
-            state.auth = false;
-        }
-    }
+const store = configureStore({
+    reducer: {user}
 })
 
-export const { setUser, resetUser } = userSlice.actions;
-
-export default userSlice.reducer;
+export default store;
