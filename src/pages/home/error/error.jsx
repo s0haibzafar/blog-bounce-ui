1fceded6e0483ee0bd70559633948312714edfd0
S0haibZafar <sohaibzafar71@gmail.com>
2023-12-25 17:57:37 +0500

    Create non found page

---
+++ src/pages/home/error/error.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+import styles from './error.module.css'
+
+function Errorr() {
+
+    return <div className={styles.errorWrapper}>
+        <div className={styles.errorHeader} >Error 404 - Page Not Found</div>
+        <div className={styles.errorBody} >
+            Go back to <Link to="/" className={styles.homeLink} >Home</Link>
+        </div>
+    </div>
+}
+
+export default Errorr;
