ea11e6931bca1852bfd5a110d027db5a31775d4d
S0haibZafar <sohaibzafar71@gmail.com>
2023-12-26 17:28:19 +0500

    Add validation using yup schema & add styling

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,14 +1,42 @@
 import styles from "./login.module.css"
 import TextInput from "../../components/textInput/textInput";
+import loginSchema from "../../schemas/loginschema";
+import { useFormik } from "formik";
 
-function Login(){
-    return(
+function Login() {
+
+    const { values, touched, handleBlur, handleChange, errors } = useFormik({
+        initialValues: {
+            username: '',
+            password: ''
+        },
+        validationSchema: loginSchema
+    })
+    return (
         <div className={styles.loginWrapper}>
             <div className={styles.loginHeader} >Log in to your account</div>
-            <TextInput />
-            <TextInput />
-            <button></button>
-            <span>Don't have an account? <button>Register</button> </span>
+            <TextInput
+                type="text"
+                name="username"
+                placeholder="username"
+                value={values.username}
+                onBlur={handleBlur}
+                onChange={handleChange}
+                error={errors.username && touched.username ? 1 : undefined}
+                errormessage={errors.username}
+            />
+            <TextInput
+                type="password"
+                name="password"
+                placeholder="password"
+                value={values.password}
+                onBlur={handleBlur}
+                onChange={handleChange}
+                error={errors.password && touched.password ? 1 : undefined}
+                errormessage={errors.password}
+            />
+            <button className={styles.loginButton} >Login </button>
+            <span >Don't have an account? <button className={styles.createAccount} >Register</button> </span>
 
         </div>
     );
