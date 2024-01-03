066a7a1dde0ef91ad336695c066ad9037b9cba18
S0haibZafar <sohaibzafar71@gmail.com>
2024-01-03 22:28:53 +0500

    Create Card for articles on home page

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,6 +1,40 @@
-function Home(){
-    return(
-        <h2>Home</h2>
+import { useState, useEffect } from "react";
+import { getNews } from "../../api/external";
+import styles from "./home.module.css";
+
+function Home() {
+    const [articals, setArticals] = useState([]);
+
+    useEffect(() => {
+        (async function newsApiCall() {
+            const response = await getNews();
+            setArticals(response);
+        })();
+
+        //cleanup function 
+        setArticals([])
+    }, []);
+
+    const handleCardClick = (articleUrl) => {
+        window.open(articleUrl, "_blank")
+    }
+
+    return (
+        <>
+            <div className={styles.header} >Latest Articles</div>
+            <div className={styles.grid} >
+                {articals.map((item) => (
+                    <div
+                        className={styles.card}
+                        key={item.url}
+                        onClick={() => handleCardClick(item.url)}
+                    >
+                        <img src={item.urlToImage} alt="" />
+                        <h3>{item.title}</h3>
+                    </div>
+                ))}
+            </div>
+        </>
     );
 }
 
