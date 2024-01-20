09d6114244a62b3d27b09653acba76545fa6bf1b
S0haibZafar <sohaibzafar71@gmail.com>
2024-01-20 22:21:27 +0500

    Add table to show crypto coin content

--- src/pages/cryptocoin/cryptocoin.jsx
+++ src/pages/cryptocoin/cryptocoin.jsx
@@ -1,14 +1,48 @@
-import { Link } from 'react-router-dom';
-import styles from './error.module.css'
+import { useState, useEffect } from "react";
+import Loader from "../../components/loader/loader";
+import styles from './cryptocoin.module.css';
+import { getCrypto } from "../../api/external";
 
 function Cryptocoin() {
 
-    return <div className={styles.errorWrapper}>
-        <div className={styles.errorHeader} >Error 404 - Page Not Found</div>
-        <div className={styles.errorBody} >
-            Go back to <Link to="/" className={styles.homeLink} >Home</Link>
-        </div>
-    </div>
+    const [data, setDatas] = useState([]);
+
+    useEffect(() => {
+        //IIFE : immediately invoked function expression
+        (async function cryptoApiCall() {
+            let response = await getCrypto();
+            setDatas(response);
+        })();
+
+        //clean up
+        setDatas([]);
+
+    }, []);
+
+    if (data.length === 0) {
+        return (<Loader text="cryptocurrencies" />)
+    }
+
+    return (
+        <table className={styles.table}>
+            <thead >
+                <tr className={styles.head}>
+                    <th>#</th>
+                    <th>Coin</th>
+                    <th>Symbol</th>
+                    <th>Price</th>
+                    <th>24h</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data.map((coin) => (
+                    <tr key={coin.id}  className={styles.tableRow} >x
+                     <td>{coin.market_cap_rank}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
 }
 
 export default Cryptocoin;
